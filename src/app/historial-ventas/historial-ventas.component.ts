228e98e8022544a4a134222ea1b943da8fd595db
yirex <mraibel12@gmail.com>
2024-07-10 19:30:19 -0400

    componente historial-venta

---
+++ src/app/historial-ventas/historial-ventas.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { PedidosService } from '../servicios/pedidos.service';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-historial-ventas',
+  templateUrl: './historial-ventas.component.html',
+  styleUrl: './historial-ventas.component.css'
+})
+export class HistorialVentasComponent implements OnInit {
+
+  public pedidos: any[] = []
+  
+  constructor(
+    public pedidosServicio: PedidosService,
+    public route: ActivatedRoute
+  ) {
+    
+    this.route.paramMap.subscribe((params: any) => {
+      this.pedidosServicio.getPedidosVendedor(params.get('id')).subscribe((data: any) => {
+        this.pedidosServicio.pedidosVendedor = data
+        this.pedidos = data
+      })
+    })
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+}
